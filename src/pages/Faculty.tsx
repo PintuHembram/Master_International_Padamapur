01dd7bfc0d82cd985cd58ba2162ea7218a3a89f6
Pintu Hembram <90277086+PintuHembram@users.noreply.github.com>
2026-04-09 01:03:13 +0530

    narya

--- src/pages/Faculty.tsx
+++ src/pages/Faculty.tsx
@@ -1,6 +1,7 @@
 import pintuComputer from "@/assets/pintucomputer.png";
 import Ahalya from "@/assets/AHALYA.png";
 import bh  from "@/assets/IMG_20260108_120404.jpg"
+import nary from "@/assets/naray.png"
 import { Layout } from "@/components/Layout";
 import { Input } from "@/components/ui/input";
 import { Mail, Search } from "lucide-react";
@@ -12,7 +13,7 @@ const facultyData = [
   { name: "Mrs. Ahalya Behera", role: "Principal", department: "Administration", qualification: "Ph.D., M.Ed.", image: Ahalya, email: "principal@masterinternational.edu" },
   { name: "Er. Pintu Hembram", role: "Computer HOD", department: "Administration", qualification: "Diploma, B.Tech.", image: pintuComputer, email: "pintuhembram@outlook.com" },
   { name: "Miss. Bhagyashree Jena", role: "Head of Primary", department: "Primary", qualification: "M.A., B.Ed.", image: bh, email: "bh@gmail.com" },
-  { name: "Mr. Narayan Das", role: "PRT - English", department: "Primary", qualification: "B.A., B.Ed.", image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=200&h=200&fit=crop", email: "" },
+  { name: "Mr. Narayan Das", role: "PRT - English", department: "Primary", qualification: "B.A., B.Ed.", image: nary, email: "narayan.das@masterinternational.edu" },
   { name: "Mr. Ranjan Kumar Sahoo", role: "Founder MIS", department: "Secondary", qualification: "M.Sc., B.Ed.", image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=200&h=200&fit=crop", email: "" },
   { name: "Mr. Sujit Kumar Das", role: "TGT - Science", department: "Secondary", qualification: "M.Sc., B.Ed.", image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=200&h=200&fit=crop", email: "" },
   { name: "Miss. Tapaswini Sahoo", role: "PGT - Physics", department: "Primary", qualification: "Ph.D., M.Sc.", image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=200&h=200&fit=crop", email: "physics@masterinternational.edu" },
